caa5500e845f68172af42efc0fa8ffc1a2cfb48c
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2024-09-30 18:25:50 +0000

    101-iterateThroughObject.js

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,13 @@
+export default function iterateThroughObject(reportWithIterator) {
+  let employeeString = '';
+
+  reportWithIterator.forEach((employee, index) => {
+    employeeString += employee;
+
+    if (index !== reportWithIterator.length - 1) {
+      employeeString += ' | ';
+    }
+  });
+
+  return employeeString;
+}
